e82c79fbdffad2f7c1a10e3b314021acd8efb48f
IEM Group <a.caballero@iemgroup.com>
2022-10-06 16:04:47 +0200

    test

--- packages/button/src/assets/index.tsx
+++ packages/button/src/assets/index.tsx
@@ -2,7 +2,7 @@ import { ButtonType, Variants } from "./Button.propsTypes"
 
 export function Button({ children, variant, color, ...props }: ButtonType) {
   return (
-    <button type="button" style={{ background: color }} {...props}>
+    <button type="submit" style={{ background: color }} {...props}>
       {children}
     </button>
   )
