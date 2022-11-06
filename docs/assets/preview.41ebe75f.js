b9e4af21dcf0ff65aca9811b8286fa783112a624
andresvcc <43545168+andresvcc@users.noreply.github.com>
2022-11-06 17:09:21 +0000

    Deploying to gh-pages from @ andresvcc/red-macaw-ui@e4360412f2f1e5e4ca39ace3347818f0550c72ca 🚀

--- docs/assets/preview.41ebe75f.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}},r={globalArg:{options:["A","B"],control:"select"}},t={globalArg:"A"};export{r as argTypes,t as args,o as parameters};
-//# sourceMappingURL=preview.41ebe75f.js.map
